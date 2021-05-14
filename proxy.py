5e5e3792464567c20688b5d89fdf40d32b0d223d
Serge Gotsuliak <sgotsulyak@gmail.com>
2021-05-14 22:39:37 +0300

    Fix Proxy

--- proxy.py
+++ proxy.py
@@ -2,19 +2,38 @@ from log import log
 from subprocess import PIPE, Popen
 from threading  import Thread
 from queue import Queue, Empty
+import sys
+
+#depth = 0
+#if len(sys.argv) > 1:
+#    depth = int(sys.argv[1])
+
+engine = "C:\\git\\kaissa64\\engines\\" + sys.argv[1]     
+depth = sys.argv[2]
+
+#cmd = ['pypy3.exe', 'C:\\git\\kaissa64\\uci.py']
+#cmd = ['C:\\git\\kaissa64\\engines\\sos.exe']
+cmd = [engine]
 
 def inProcess(stdin, queue):
     while True:
         msg = queue.get()
+
+        # Hacking Arena :)
+        if msg[0:2] == "go":
+            msg += f" depth {depth}"
+            log(f"[:] {msg}", "proxy.log")                    
+
         stdin.write(msg + "\n")
         stdin.flush()
 
 def outProcess(stdout, queue):
     while True:
         for line in iter(stdout.readline, b''):
-            print(line.rstrip())
+            log(f"{line.rstrip()}", "proxy.log")        
+            print(line.rstrip(), flush=True)
 
-process = Popen(['sos.exe'], text=True, bufsize=0, stdin=PIPE, stdout=PIPE)
+process = Popen(cmd, text=True, stdin=PIPE, stdout=PIPE)
 
 queue = Queue()
 t1 = Thread(target=inProcess, args=(process.stdin, queue))
@@ -25,11 +44,15 @@ t2 = Thread(target=outProcess, args=(process.stdout, queue))
 t2.daemon = True # thread dies with the program
 t2.start()
 
+log(f"\n[START]\n", "proxy.log")        
+
 while True:
 
     msg = input()
-    log(f">>> {msg}", "proxy.log")    
-    if msg == "quit": break
+    log(f">>> {msg}", "proxy.log")        
     queue.put(msg)
+    if msg == "quit": break
 
 process.terminate()
+
+log(f"\n[FINISH]\n", "proxy.log")        
